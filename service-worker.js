2fe510f70a1ac147c206a714eec8660ea581c23b
Ladawalpu <puladawal.s@gmail.com>
2026-08-10 12:14:52 +0700

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 // © 2026 Puladawal S. (puladawal.s@gmail.com) — MNST Hospital. All Rights Reserved. Unauthorized copying, redistribution, or modification of this application is prohibited without written permission.
 
-const CACHE_NAME = 'aneshub-cache-v19';
+const CACHE_NAME = 'aneshub-cache-v20';
 const ASSETS = [
   './index.html',
   './anesth-cal.html',
